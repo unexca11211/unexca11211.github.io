130b386ab02c8fa2b9d3ba79a90bb8fe656109cf
Douglas Barreto <dosideveloper@gmail.com>
2023-07-06 20:07:04 -0400

    Feature: Carrousel

--- src/App.js
+++ src/App.js
@@ -1,22 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import Slider from "./Slider";
 
 function App() {
+  const data = [
+    {
+      title: "lorem ipsum titulo",
+      description: "una description sencilla",
+      url: "https://images.pexels.com/photos/11631746/pexels-photo-11631746.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load",
+    },
+    {
+      title: "lorem ipsum titulo2",
+      description: "una description sencilla",
+      url: "https://images.pexels.com/photos/15857477/pexels-photo-15857477/free-photo-of-tunel-pavimento-interior-vacio.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load",
+    },
+    {
+      title: "lorem ipsum titulo3",
+      description: "una description sencilla",
+      url: "http:///localhost:3000/favicon.ico",
+    },
+    {
+      title: "lorem ipsum titulo4",
+      description: "una description sencilla",
+      url: "https://images.pexels.com/photos/16159464/pexels-photo-16159464/free-photo-of-edificio-cielo-azul-urbano-pueblo.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load",
+    },
+    {
+      title: "lorem ipsum titulo5",
+      description: "una description sencilla",
+      url: "https://images.pexels.com/photos/15109908/pexels-photo-15109908/free-photo-of-flores-insecto-mariposa-floracion.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load",
+    },
+    {
+      title: "lorem ipsum titulo6",
+      description: "una description sencilla",
+      url: "http:///localhost:3000/logo192.png",
+    },
+  ];
+  const containerStyles = {
+    width: "90vw",
+    height: "90vh",
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header style={containerStyles}>
+        <Slider data={data} />
       </header>
     </div>
   );
